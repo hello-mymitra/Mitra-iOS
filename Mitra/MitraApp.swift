60f09911a1bd34f6c20b334ef493af0411d3b589
“Shiv <stavangandhi3008@gmail.com>
2025-02-26 11:23:09 -0600

    Project Init

--- Mitra/MitraApp.swift
+++ Mitra/MitraApp.swift
@@ -8,13 +8,20 @@
 import SwiftUI
 
 @main
-struct MitraApp: App {
-    let persistenceController = PersistenceController.shared
-
+struct SagaApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    static var orientationLock = UIInterfaceOrientationMask.portrait
+    
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return AppDelegate.orientationLock
     }
 }
